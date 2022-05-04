d9f220dc863cead12b34094d02336bce486a7a1c
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-04 09:49:51 +0430

    database implemented

--- internal/database/redis.go
+++ internal/database/redis.go
@@ -10,6 +10,7 @@ type Database struct {
 	redis *redis.Client
 }
 
+// Register : creates a new redis connection
 func (db *Database) Register(cfg Config) {
 	db.redis = redis.NewClient(&redis.Options{
 		Addr:     cfg.Address,
@@ -18,14 +19,23 @@ func (db *Database) Register(cfg Config) {
 	})
 }
 
+// Add : add a new item to redis
 func (db *Database) Add(key string, value string) error {
 	ctx := context.Background()
 
 	return db.redis.Set(ctx, key, value, 0).Err()
 }
 
+// Get : get a record from redis
 func (db *Database) Get(key string) (string, error) {
 	ctx := context.Background()
 
 	return db.redis.Get(ctx, key).Result()
+}
+
+// Delete : remove a record from redis
+func (db *Database) Delete(key string) error {
+	ctx := context.Background()
+
+	return db.redis.Del(ctx, key).Err()
 }
