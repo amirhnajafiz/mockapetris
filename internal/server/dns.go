96ba753bd374ba7d08996fa5c5ef543eeb8fe33a
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-04 09:26:01 +0430

    dns server init

--- internal/server/dns.go
+++ internal/server/dns.go
@@ -1,1 +1,35 @@
 package server
+
+import (
+	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+)
+
+var records map[string]string
+
+func Start() {
+	records = map[string]string{
+		"google.com": "216.58.196.142",
+		"amazon.com": "176.32.103.205",
+	}
+
+	//Listen on UDP Port
+	addr := net.UDPAddr{
+		Port: 8090,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+	u, _ := net.ListenUDP("udp", &addr)
+
+	// Wait to get request on that port
+	for {
+		tmp := make([]byte, 1024)
+		_, addr, _ := u.ReadFrom(tmp)
+		clientAddr := addr
+		packet := gopacket.NewPacket(tmp, layers.LayerTypeDNS, gopacket.Default)
+		dnsPacket := packet.Layer(layers.LayerTypeDNS)
+		tcp, _ := dnsPacket.(*layers.DNS)
+		serveDNS(u, clientAddr, tcp)
+	}
+}
