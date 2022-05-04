aefae2d4cf4e29f0b39404ca9d8b9acfe130d80e
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-04 09:32:49 +0430

    server of server dns implemented

--- internal/server/dns.go
+++ internal/server/dns.go
@@ -9,61 +9,38 @@ import (
 	"github.com/google/gopacket/layers"
 )
 
-var records map[string]string
-
-func Start() {
-	records = map[string]string{
-		"google.com": "216.58.196.142",
-		"amazon.com": "176.32.103.205",
-	}
-
-	//Listen on UDP Port
-	addr := net.UDPAddr{
-		Port: 8090,
-		IP:   net.ParseIP("127.0.0.1"),
-	}
-	u, _ := net.ListenUDP("udp", &addr)
-
-	// Wait to get request on that port
-	for {
-		tmp := make([]byte, 1024)
-		_, addr, _ := u.ReadFrom(tmp)
-		clientAddr := addr
-		packet := gopacket.NewPacket(tmp, layers.LayerTypeDNS, gopacket.Default)
-		dnsPacket := packet.Layer(layers.LayerTypeDNS)
-		tcp, _ := dnsPacket.(*layers.DNS)
-		serveDNS(u, clientAddr, tcp)
-	}
-}
-
 func serveDNS(u *net.UDPConn, clientAddr net.Addr, request *layers.DNS) {
-	replyMess := request
-	var dnsAnswer layers.DNSResourceRecord
-	dnsAnswer.Type = layers.DNSTypeA
-	var ip string
-	var err error
-	var ok bool
-	ip, ok = records[string(request.Questions[0].Name)]
+	ip, ok := Records[string(request.Questions[0].Name)]
 	if !ok {
 		log.Println("no records found")
 	}
+
 	a, _, _ := net.ParseCIDR(ip + "/24")
-	dnsAnswer.Type = layers.DNSTypeA
-	dnsAnswer.IP = a
-	dnsAnswer.Name = request.Questions[0].Name
+
+	dnsAnswer := layers.DNSResourceRecord{
+		Type:  layers.DNSTypeA,
+		IP:    a,
+		Name:  request.Questions[0].Name,
+		Class: layers.DNSClassIN,
+	}
+
 	fmt.Println(request.Questions[0].Name)
-	dnsAnswer.Class = layers.DNSClassIN
+
+	replyMess := request
 	replyMess.QR = true
 	replyMess.ANCount = 1
 	replyMess.OpCode = layers.DNSOpCodeNotify
 	replyMess.AA = true
 	replyMess.Answers = append(replyMess.Answers, dnsAnswer)
 	replyMess.ResponseCode = layers.DNSResponseCodeNoErr
+
 	buf := gopacket.NewSerializeBuffer()
 	opts := gopacket.SerializeOptions{}
-	err = replyMess.SerializeTo(buf, opts)
+	err := replyMess.SerializeTo(buf, opts)
+
 	if err != nil {
 		panic(err)
 	}
+
 	_, _ = u.WriteTo(buf.Bytes(), clientAddr)
 }
