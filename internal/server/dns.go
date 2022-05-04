efa634fbdce74afac80063bda92265e8d9fd65cc
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-04 09:28:05 +0430

    server dns added

--- internal/server/dns.go
+++ internal/server/dns.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"fmt"
+	"log"
 	"net"
 
 	"github.com/google/gopacket"
@@ -32,4 +34,36 @@ func Start() {
 		tcp, _ := dnsPacket.(*layers.DNS)
 		serveDNS(u, clientAddr, tcp)
 	}
+}
+
+func serveDNS(u *net.UDPConn, clientAddr net.Addr, request *layers.DNS) {
+	replyMess := request
+	var dnsAnswer layers.DNSResourceRecord
+	dnsAnswer.Type = layers.DNSTypeA
+	var ip string
+	var err error
+	var ok bool
+	ip, ok = records[string(request.Questions[0].Name)]
+	if !ok {
+		log.Println("no records found")
+	}
+	a, _, _ := net.ParseCIDR(ip + "/24")
+	dnsAnswer.Type = layers.DNSTypeA
+	dnsAnswer.IP = a
+	dnsAnswer.Name = request.Questions[0].Name
+	fmt.Println(request.Questions[0].Name)
+	dnsAnswer.Class = layers.DNSClassIN
+	replyMess.QR = true
+	replyMess.ANCount = 1
+	replyMess.OpCode = layers.DNSOpCodeNotify
+	replyMess.AA = true
+	replyMess.Answers = append(replyMess.Answers, dnsAnswer)
+	replyMess.ResponseCode = layers.DNSResponseCodeNoErr
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{}
+	err = replyMess.SerializeTo(buf, opts)
+	if err != nil {
+		panic(err)
+	}
+	_, _ = u.WriteTo(buf.Bytes(), clientAddr)
 }
