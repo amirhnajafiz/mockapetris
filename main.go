352eac193fecfcfe812e05362157f8656a27a0ba
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-04 11:21:58 +0430

    main refactor

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/amirhnajafiz/mockapetris/internal/dns"
+import (
+	"github.com/amirhnajafiz/mockapetris/internal/database"
+	"github.com/amirhnajafiz/mockapetris/internal/dns"
+)
 
 func main() {
-	dns.Start(dns.Config{})
+	db := database.Database{}
+	db.Register(database.Config{})
+
+	server := dns.Server{
+		Dns: dns.DNS{
+			DB: db,
+		},
+	}
+
+	server.Start(dns.Config{})
 }
