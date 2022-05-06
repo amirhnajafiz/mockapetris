6f5192e0e3cc8c3c9270b485bfae0f53947a41e3
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-06 18:21:46 +0430

    config default method added

--- internal/config/default.go
+++ internal/config/default.go
@@ -1,1 +1,19 @@
 package config
+
+import (
+	"github.com/amirhnajafiz/mockapetris/internal/database"
+	"github.com/amirhnajafiz/mockapetris/internal/dns"
+)
+
+func Default() Config {
+	return Config{
+		Database: database.Config{
+			Address:  "",
+			Password: "",
+		},
+		Server: dns.Config{
+			IP:   "",
+			Port: 8090,
+		},
+	}
+}
