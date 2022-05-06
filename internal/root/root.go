9ac89d7e66dd359357d8c322403bc40347709f24
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-06 18:36:08 +0430

    root methods implemented

--- internal/root/root.go
+++ internal/root/root.go
@@ -1,11 +1,42 @@
 package root
 
-import "github.com/amirhnajafiz/mockapetris/internal/database"
+import (
+	"net/http"
+
+	"github.com/amirhnajafiz/mockapetris/internal/database"
+)
 
 type Root struct {
-	DB database.Database
+	DB      database.Database
+	Srv     *http.ServeMux
+	Address string
 }
 
-func (r Root) Register() {
+func (r Root) Register(d database.Database) Root {
+	srv := http.NewServeMux()
+
+	srv.HandleFunc("/put", r.AddRecord)
+	srv.HandleFunc("/del", r.RemoveRecord)
+
+	return Root{
+		DB:      d,
+		Srv:     srv,
+		Address: ":1348",
+	}
+}
+
+func (r Root) Start() {
+	go func() {
+		if err := http.ListenAndServe(r.Address, r.Srv); err != nil {
+			panic(err)
+		}
+	}()
+}
+
+func (r Root) AddRecord(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (r Root) RemoveRecord(w http.ResponseWriter, req *http.Request) {
 
 }
