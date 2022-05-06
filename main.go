e6a59fc62cdbbc3901804c179a9fd8507cb139da
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-06 18:23:02 +0430

    server config init

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"github.com/amirhnajafiz/mockapetris/internal/config"
 	"github.com/amirhnajafiz/mockapetris/internal/database"
 	"github.com/amirhnajafiz/mockapetris/internal/dns"
 )
 
 func main() {
+	cfg := config.Load()
+
 	db := database.Database{}
-	db.Register(database.Config{})
+	db.Register(cfg.Database)
 
 	dns.Server{
 		Dns: dns.DNS{
 			DB: db,
 		},
-	}.Start(dns.Config{})
+	}.Start(cfg.Server)
 }
