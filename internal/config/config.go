0a64fe97fdf893cb16492f20de1ddf110195f275
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-06 18:20:30 +0430

    config type init

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,11 @@
 package config
+
+import (
+	"github.com/amirhnajafiz/mockapetris/internal/database"
+	"github.com/amirhnajafiz/mockapetris/internal/dns"
+)
+
+type Config struct {
+	Database database.Config `koanf:"cache"`
+	Server   dns.Config      `koanf:"dns"`
+}
