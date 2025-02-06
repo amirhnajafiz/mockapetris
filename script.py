0ced63d16a3f36bbc9b3ae3981acfcabfa78e5e2
amirhnajafiz <najafizadeh21@gmail.com>
2025-02-06 18:00:04 -0500

    refactor: resolver logic

--- script.py
+++ script.py
@@ -5,18 +5,20 @@ from src.resolver import DNSResolver
 
 
 
-# Read roots from `roots.json`
-roots = {}
+# read roots from `roots.json`
 with open('roots.json', 'r') as f:
     roots = json.load(f)
 
 domain = "cs.stonybrook.edu"
 qtype = "A"
 
-resolver = DNSResolver(roots, domain, qtype)
+if __name__ == "__main__":
+    # create a resolver instance
+    resolver = DNSResolver(roots, domain, qtype)
 
-ans, ok = resolver.resolve()
-if not ok:
-    sys.exit(1)
-else:
-    print(ans)
+    # get the answer
+    ans, ok = resolver.resolve()
+    if not ok:
+        sys.exit(1)
+    else:
+        print(ans)
--- src/resolver.py
+++ src/resolver.py
@@ -1,5 +1,6 @@
 import dns.message
 import dns.query
+
 from .utils import qtype_map
 
 
@@ -14,43 +15,38 @@ for DNS resolve.
 """
 class DNSResolver:
     def __init__(self, roots : dict, domain : str, qtype : str):
-        self.index = 0
-        self.roots = list(roots.items())
+        self.roots = roots
         self.query = dns.message.make_query(domain, qtype_map(qtype))
 
     def resolve(self) -> tuple[dns.message.Message, bool]:
-        # check the index limit
-        if self.index == len(self.roots):
-            return None, False
+        # iterate over roots
+        for root_ip in self.roots.values():
+            # call the root DNS
+            response = dns.query.udp(self.query, root_ip)
 
-        # get one root address
-        root_ip = self.roots[self.index][1]
-        self.index += 1
+            # if the root response is empty
+            if len(response.answer) == 0 and len(response.authority) == 0:
+                continue
 
-        # call the root DNS
-        response = dns.query.udp(self.query, root_ip)
-        if len(response.answer) == 0 and len(response.authority) == 0:
-            return None, True
+            # loop variables
+            tld = ""
+            tld_ip = ""
 
-        # loop variables
-        tld = ""
-        tld_ip = ""
+            while True:
+                # check the response for answer
+                if len(response.answer) > 0:
+                    return response, True
 
-        while True:
-            # check the response for answer
-            if len(response.answer) > 0:
-                return response, True
+                # select authority and its IP
+                tld = response.authority[0][0].to_text()
 
-            # select authority and its IP
-            tld = response.authority[0][0].to_text()
-            print(tld)
-
-            # get the IP of NS from additional
-            for addi in response.additional:
-                if addi.name.to_text() == tld:
-                    print(addi[0].to_text())
-                    tld_ip = addi[0].to_text()
-                    break
-            
-            # make a new call to the next
-            response = dns.query.udp(self.query, tld_ip)
+                # get the IP of NS from additional
+                for addi in response.additional:
+                    if addi.name.to_text() == tld:
+                        tld_ip = addi[0].to_text()
+                        break
+                
+                # make a new call to the next
+                response = dns.query.udp(self.query, tld_ip)
+        
+        return None, False
