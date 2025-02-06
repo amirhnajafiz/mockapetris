eb9d7f7f60d7dff79c064b312e8af59404c0408d
amirhnajafiz <najafizadeh21@gmail.com>
2025-02-06 17:25:29 -0500

    add: roots.json for dns root servers

--- script.py
+++ script.py
@@ -1,39 +1,49 @@
 import dns.message
 import dns.query
 import dns.rdatatype
+import json
+import sys
 
 
 
-# Choose a root name server (one of the 13 root servers)
-ROOT_SERVER = "198.41.0.4"  # Example: a.root-servers.net
+# Read roots from `roots.json`
+roots = {}
+with open('roots.json', 'r') as f:
+    roots = json.load(f)
 
-# Create a DNS query message for "example.com" (A record)
-domain = "cs.stonybrook.edu"
-query = dns.message.make_query(domain, dns.rdatatype.MX)
+print(roots)
 
-# Send query directly to the root name server
-response = dns.query.udp(query, ROOT_SERVER)
-tdl = ""
-ip = ""
+for key in roots:
+    # Choose a root name server (one of the 13 root servers)
+    ROOT_SERVER = roots[key]  # Example: a.root-servers.net
 
-while True:
-    # Check the response for answer
-    if len(response.answer) > 0:
-        print(response)
-        break
+    # Create a DNS query message for "example.com" (A record)
+    domain = "cs.stonybrook.edu"
+    query = dns.message.make_query(domain, dns.rdatatype.A)
 
-    # Select Authority and its IP
-    tld = response.authority[0][0].to_text()
-    print(tld)
-
-    # Get the IP of NS from additional
+    # Send query directly to the root name server
+    response = dns.query.udp(query, ROOT_SERVER)
+    tdl = ""
     ip = ""
-    for addi in response.additional:
-        if addi.name.to_text() == tld:
-            print(addi[0].to_text())
-            ip = addi[0].to_text()
-            break
+
+    while True:
+        # Check the response for answer
+        if len(response.answer) > 0:
+            print(response)
+            sys.exit(0)
+
+        # Select Authority and its IP
+        tld = response.authority[0][0].to_text()
+        print(tld)
+
+        # Get the IP of NS from additional
+        ip = ""
+        for addi in response.additional:
+            if addi.name.to_text() == tld:
+                print(addi[0].to_text())
+                ip = addi[0].to_text()
+                break
 
 
-    tmp = dns.query.udp(query, ip)
-    response = tmp
+        tmp = dns.query.udp(query, ip)
+        response = tmp
